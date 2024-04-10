89acf245bc7608e37514e1c2688c1c75ccb6c262
Lê Đỗ Thành Đạt <86953496+dat1208@users.noreply.github.com>
2024-04-11 02:22:44 +0700

    aw

--- Admin/replaceBlockURL.php
+++ Admin/replaceBlockURL.php
@@ -1,13 +1,13 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-   // thu thập dữ liệu form
-   $editor = $_POST['editor'];
-   // thay thế thẻ </p> với ký tự xuống dòng
-   $editor = str_replace('</p>', "\n", $editor);
-   // Loại bỏ các thẻ HTML còn lại
-   $editor = strip_tags($editor);
-   // Ghi nội dung vào file
-   file_put_contents('/path/to/your/file.txt', $editor);
+  // thu thập dữ liệu form
+  $editor = $_POST['editor'];
+  // thay thế thẻ </p> với ký tự xuống dòng
+  $editor = str_replace('</p>', "\r\n", $editor);
+  // Loại bỏ các thẻ HTML còn lại
+  $editor = strip_tags($editor);
+  // Ghi nội dung vào file
+  file_put_contents('/path/to/your/file.txt', $editor);
   header('Location: ' . $_SERVER['HTTP_REFERER']);
 }
 ?>
