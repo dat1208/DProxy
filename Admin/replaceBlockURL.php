d85dddc8fd9da26ccb9c6199901b3b2eae38f927
Lê Đỗ Thành Đạt <86953496+dat1208@users.noreply.github.com>
2024-04-11 02:05:20 +0700

    a

--- Admin/replaceBlockURL.php
+++ Admin/replaceBlockURL.php
@@ -3,7 +3,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   // thu thập dữ liệu form
   $editor = $_POST['editor'];
   // write the input to the blocked_sites.txt file
-  $output = shell_exec('sudo echo "ok ok ok" > /etc/squid/blocked_sites.txt');
+  file_put_contents('/etc/squid/blocked_sites.txt', $editor);
   echo "Chào mừng, " . $editor;
 }
 ?>
