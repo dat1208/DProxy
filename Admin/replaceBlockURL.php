f32aa1f20e31139e68eae4ee705e3039efe6cd74
Lê Đỗ Thành Đạt <86953496+dat1208@users.noreply.github.com>
2024-04-11 02:37:12 +0700

    a

--- Admin/replaceBlockURL.php
+++ Admin/replaceBlockURL.php
@@ -2,8 +2,10 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   // thu thập dữ liệu form
   $editor = $_POST['editor'];
-  // write the input to the blocked_sites.txt file
-  file_put_contents('/etc/squid/blocked_sites.txt', $editor);
+  // Làm sạch và loại bỏ các thẻ HTML
+  $editor = strip_tags(htmlspecialchars_decode($editor, ENT_QUOTES));
+  // Ghi nội dung vào file
+  file_put_contents('/path/to/your/file.txt', $editor);
   header('Location: ' . $_SERVER['HTTP_REFERER']);
 }
 ?>
