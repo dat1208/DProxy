a00f09df9287c2d50c8dc71831dadacb8e9abaf3
Lê Đỗ Thành Đạt <86953496+dat1208@users.noreply.github.com>
2024-04-16 18:45:38 +0700

    a

--- Admin/readBlockURL.php
+++ Admin/readBlockURL.php
@@ -3,7 +3,7 @@
 
     if(file_exists($file_path)) {
         $content = file_get_contents($file_path);
-
+        $content = nl2br($content);  // Chuyển đổi ký tự xuống dòng thành thẻ <br>
         echo $content;
     } else {
         echo "File không tồn tại.";
