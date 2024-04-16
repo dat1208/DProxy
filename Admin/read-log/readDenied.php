c076478ea8fa2ce83c10e1726c6e420fb596a889
Lê Đỗ Thành Đạt <86953496+dat1208@users.noreply.github.com>
2024-04-16 17:45:12 +0700

    a

--- Admin/read-log/readDenied.php
+++ Admin/read-log/readDenied.php
@@ -1,6 +1,6 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $command = 'python /home/log.py -d 2>&1';
+    $command = 'sudo python /home/log.py -d 2>&1';
     $output = shell_exec($command);
     echo "<pre>$output</pre>";
 }
