373e25083cec8a3508d1edc4186fb36bd57585d1
Lê Đỗ Thành Đạt <86953496+dat1208@users.noreply.github.com>
2024-04-16 18:01:11 +0700

    Update readTotalBandwidthPerIP.php

--- Admin/read-log/readTotalBandwidthPerIP.php
+++ Admin/read-log/readTotalBandwidthPerIP.php
@@ -2,6 +2,6 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $command = 'python /root/log.py -a 2>&1';
     $output = shell_exec($command);
-    echo "<p>$output</p>";
+    echo "<pre>$output</pre>";
 }
 ?>
