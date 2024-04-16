71fda660a218038366a7c7588d1abb8a8902223d
Lê Đỗ Thành Đạt <86953496+dat1208@users.noreply.github.com>
2024-04-16 18:49:32 +0700

    a

--- Admin/replaceAllowIP.php
+++ Admin/replaceAllowIP.php
@@ -3,8 +3,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   // thu thập dữ liệu form
   $editor = $_POST['editor'];
   
-  $result = str_replace(['<p>', '</p>'], "\n", $editor);
-  // write the input to the blocked_sites.txt file
+  $result = str_replace(['<p>', '</p>', '<br>', '&nbsp;'], "\n", $editor);  // write the input to the blocked_sites.txt file
   file_put_contents('/etc/squid/allowed_ips.txt', $result);
   header('Location: ' . $_SERVER['HTTP_REFERER']);
 }
--- Admin/replaceBlockURL.php
+++ Admin/replaceBlockURL.php
@@ -3,7 +3,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   // thu thập dữ liệu form
   $editor = $_POST['editor'];
   
-  $result = str_replace(['<p>', '</p>'], "\n", $editor);
+  $result = str_replace(['<p>', '</p>', '<br>', '&nbsp;'], "\n", $editor);
   // write the input to the blocked_sites.txt file
   file_put_contents('/etc/squid/blocked_sites.txt', $result);
   header('Location: ' . $_SERVER['HTTP_REFERER']);
