c1dc36d2db2033faac18784cc3a35bbfa923522c
Lê Đỗ Thành Đạt <86953496+dat1208@users.noreply.github.com>
2024-04-16 15:56:57 +0700

    Update readAllowIP.php

--- Admin/readAllowIP.php
+++ Admin/readAllowIP.php
@@ -1,5 +1,5 @@
 <?php
-    $file_path = '/etc/squid/allowed_ips.txt.txt';
+    $file_path = '/etc/squid/allowed_ips.txt';
 
     if(file_exists($file_path)) {
         $content = file_get_contents($file_path);
