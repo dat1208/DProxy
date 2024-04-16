b74811caa4b2611c46b96959e3e9ad88e5c85c15
Lê Đỗ Thành Đạt <86953496+dat1208@users.noreply.github.com>
2024-04-16 17:48:32 +0700

    a

--- Admin/read-log/readTotalBandwidthPerIP.php
+++ Admin/read-log/readTotalBandwidthPerIP.php
@@ -1,6 +1,6 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $command = 'sudo python /home/log.py -a 2>&1';
+    $command = 'python /root/log.py -a 2>&1';
     $output = shell_exec($command);
     echo "<pre>$output</pre>";
 }
