9b2899afa749b6a64052ba67d181b635faa25d13
Lê Đỗ Thành Đạt <86953496+dat1208@users.noreply.github.com>
2024-04-16 18:16:08 +0700

    Update readTotalBandwidthPerIP.php

--- Admin/read-log/readTotalBandwidthPerIP.php
+++ Admin/read-log/readTotalBandwidthPerIP.php
@@ -1,6 +1,7 @@
 <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $output = shell_exec('python /root/log.py -a');
+        $output = preg_replace('/\x1b\[[0-9;]*m/', '', $output);  // Loại bỏ mã điều khiển ANSI
     }
-    echo "<pre>$output</pre>";
+    echo $output;
 ?>
